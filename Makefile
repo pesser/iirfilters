dfe75921ec1414feb1b8fbe78de6f68e5b8ad4c6
acclib02 <acclib02@creek07.ziti.uni-heidelberg.de>
2016-07-31 16:15:50 +0200

    debug thrust version

--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ iirfilter: iirfilter.cxx svenpeter_convolve_iir_nosimd.cxx svenpeter_kernel_iir_
 	g++ $(CFLAGS) iirfilter.cxx svenpeter_convolve_iir_nosimd.cxx svenpeter_kernel_iir_deriche.cxx -lpng -o iirfilter
 
 iirfilter-cuda: iirfilter.cu svenpeter_kernel_iir_deriche.cxx fastfilters.hxx image.hxx
-	nvcc $(CFLAGS) iirfilter.cu -lpng -o iirfilter-cuda
+	nvcc $(CFLAGS) iirfilter.cu svenpeter_kernel_iir_deriche.cxx -lpng -o iirfilter-cuda
+
+iirfilter-thrust: iirfilter_thrust.cu svenpeter_kernel_iir_deriche.cxx fastfilters.hxx image.hxx
+	nvcc $(CFLAGS) iirfilter_thrust.cu svenpeter_kernel_iir_deriche.cxx -lpng -o iirfilter-thrust
 
 all:
-	iirfilter iirfilter-cuda 
+	iirfilter iirfilter-cuda iirfilter-thrust
--- iirfilter_thrust.cu
+++ iirfilter_thrust.cu
@@ -12,16 +12,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <thrust/functional.h>
+#include <thrust/tabulate.h>
 #include <thrust/device_vector.h>
 #include <thrust/tuple.h>
+#include <thrust/copy.h>
+#include <thrust/fill.h>
+#include <thrust/transform.h>
+#include <thrust/scan.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/transform_iterator.h>
+#include <thrust/iterator/zip_iterator.h>
+#include <thrust/random.h>
 
 typedef thrust::tuple<float, float, float, float> FloatTuple;
 
-template <typename Tuple>
-struct addfirsts : public thrust::binary_function<Tuple,Tuple,float>
+
+struct RandGen
+{
+    RandGen() {}
+
+    __host__ __device__
+    float operator () (int idx)
+    { 
+       thrust::default_random_engine randEng;
+       thrust::uniform_real_distribution<float> uniDist(-3.2, 3.2);
+       randEng.discard(idx);
+       return uniDist(randEng);
+    }
+};
+
+struct convolve_4
+{
+  const FloatTuple d;
+  const FloatTuple causal;
+
+  convolve_4( const FloatTuple _d, const FloatTuple _causal ) : d(_d), causal(_causal) {}
+
+   template <typename Tuple, typename Tuple2>
+  __host__ __device__
+  Tuple operator()(const Tuple &y, const Tuple2 &x)
+  {
+    using thrust::get;
+    float sum =  get<0>(causal) * get<0>(x)
+               + get<1>(causal) * get<1>(x)
+               + get<2>(causal) * get<2>(x)
+               + get<3>(causal) * get<3>(x)
+               - get<0>(d) * get<0>(y) 
+               - get<1>(d) * get<1>(y)
+               - get<2>(d) * get<2>(y) 
+               - get<3>(d) * get<3>(y);
+    return thrust::make_tuple(sum, get<0>(y), get<1>(y), get<2>(y));
+  }
+};
+
+struct addfirsts : public thrust::binary_function<FloatTuple, FloatTuple, float>
 {
   __host__ __device__
-  Tuple operator()( const Tuple& x, const Tuple& y ) const
+  float operator()( const FloatTuple& x, const FloatTuple& y ) const
   {
     return thrust::get<0>(x) + thrust::get<0>(y);
   }
@@ -31,102 +79,66 @@ struct row_idx : public thrust::unary_function<int, int>
 {
   const int width;
   
-  row_idx(Tuple _width) : width(_width)  {}
+  row_idx(int _width) : width(_width)  {}
   
   __host__ __device__
-  Tuple operator()( const int idx ) const
+  int operator()( const int idx ) const
   {
-    return (idx) % width;
+    return (idx) / width;
   }
 };
 
-
-template <typename Tuple>
-struct convolve_4 : public thrust::binary_function<Tuple,Tuple,Tuple>
-{
-  const Tuple d;
-  const Tuple causal;
-  convolve_4( Tuple _d, Tuple _causal ) : d(_d), causal(_causal) {}
-
-  __host__ __device__
-  Tuple operator()( const Tuple& x, const Tuple& y ) const
-  {
-    float sum = 0.0;
-    for (unsigned int j = 0; j < 4; ++j)
-      sum += thrust::get<j>(causal) * thrust::get<j>(x);
-    
-    for (unsigned int j = 0; j < 4; ++j)
-      sum -= thrust::get<j>(d) * thrust::get<j>(y);
-    
-    return thrust::make_tuple(sum,
-                              thrust::get<0>(y),
-                              thrust::get<1>(y),
-                              thrust::get<2>(y));
-  }
-};
-
-void convolve_iir_gpu( float* input, float* output, int n_cols, int n_rows,
+void convolve_iir_gpu(const float* input, float* output, int n_cols, int n_rows,
                        fastfilters::iir::Coefficients &coefs )
 {
   cudaSetDevice(0);
-  unsigned int n_pixels;
-  cudaDeviceProp prop;
-  thrust::tuple<float, float, float, float> d, causal, anticausal;
+  unsigned int n_pixels, n_border;
 
   n_pixels = n_cols * n_rows;
+  n_border = 3; // since window size = 4
 
-  for (int i=0; i<4;++i)
-    {
-      thrust::get<i>(d) = coefs.d[i]; 
-      thrust::get<i>(causal) = coefs.n_causal[i];
-      thrust::get<i>(anticausal) = coefs.n_anticausal[i];
-    }
+  FloatTuple d(coefs.d[0],coefs.d[1], coefs.d[2], coefs.d[3]);
+  FloatTuple causal(coefs.n_causal[0],coefs.n_causal[1], coefs.n_causal[2], coefs.n_causal[3]);
+  FloatTuple anticausal(coefs.n_anticausal[0],coefs.n_anticausal[1], coefs.n_anticausal[2], coefs.n_anticausal[3]);
 
-  thrust::device_vector<float> in(n_pixels+6);
+  thrust::device_vector<float> in(n_pixels+2*n_border); // left and right border 
   thrust::device_vector<float> out(n_pixels);
   
-  thrust::fill(in.begin(), in.begin()+3, 0.0);
-  thrust::copy(input, n_pixels, in.begin()+3);
-  thrust::fill(in.begin()+3+n_pixels, in.end(), 0.0);
+  // pad border with zeros
+  thrust::fill(in.begin(), in.begin()+n_border, 0.0);
+  thrust::copy_n(input, n_pixels, in.begin()+n_border);
+  thrust::fill(in.begin()+n_border+n_pixels, in.end(), 0.0);
 
-  typedef thrust::device_vector<float>::iterator FloatIterator;
-  thrust::device_vector<thrust::tuple<float, float, float, float>> forth, back;
+  FloatTuple zero(0,0,0,0);
+  thrust::device_vector<FloatTuple> forth(n_pixels,zero);
+  thrust::device_vector<FloatTuple> back(n_pixels,zero);
+
+  thrust::device_vector<int> key(n_pixels);
+  thrust::tabulate(key.begin(), key.end(), row_idx(n_cols));
+  thrust::equal_to<int> eq;
+
+  exclusive_scan_by_key(key.begin(), 
+                        key.end(),
+                        thrust::make_zip_iterator(thrust::make_tuple(in.begin()+3, in.begin()+2, in.begin()+1, in.begin()))+1,
+                        forth.begin(), 
+                        thrust::make_tuple(coefs.d[0]*in[3],0,0,0),
+                        eq,
+                        convolve_4(d,causal));
   
+  auto in_rev = in.rbegin();
   thrust::exclusive_scan_by_key
-    (thrust::make_zip_iterator
-     (thrust::make_tuple(in.begin()+3, in.begin()+2,
-                         in.begin()+1, in.begin())),
-     thrust::make_zip_iterator
-     (thrust::make_tuple(in.end()-3, in.end()-4,
-                         in.end()-5, in.end()-6)),
-     thrust::make_transform_iterator<int>
-     (thrust::make_counting_iterator<int(0), row_idx(n_cols)),
-     forth.begin(), // result
-     thrust::make_tuple(0.0, 0.0, 0.0, 0.0),
-     thrust::equal_to<int>,
-     convolve_4(d, causal));
-
-  thrust::reverse_iterator<FloatIterator> in_rev(in.end());
-    
-  thrust::exclusive_scan_by_key
-    (thrust::make_zip_iterator
-     (thrust::make_tuple(in_rev.begin()+3, in_rev.begin()+2,
-                         in_rev.begin()+1, in_rev.begin())),
-     thrust::make_zip_iterator
-     (thrust::make_tuple(in_rev.end()-3, in_rev.end()-4,
-                         in_rev.end()-5, in_rev.end()-6)),
-     thrust::make_transform_iterator<int>
-     (thrust::make_counting_iterator<int(0), row_idx(n_cols)),
-     back.begin(), // result
-     thrust::make_tuple(0.0, 0.0, 0.0, 0.0),
-     thrust::equal_to<int>,
+    (key.begin(),
+     key.end(),
+     thrust::make_zip_iterator (thrust::make_tuple(in_rev+3, in_rev+2, in_rev+1, in_rev)),
+     back.begin(),
+     thrust::make_tuple(coefs.d[0]*in_rev[3], 0.0, 0.0, 0.0),
+     eq,
      convolve_4(d, anticausal));
     
-  thrust::reverse_iterator<FloatIterator> back_rev(back.end());
-
-  transform(forth.begin(), forth.end(), back_rev.begin(), out.begin(), addfirsts);
+  auto back_rev = back.rbegin();
+  thrust::transform(forth.begin(), forth.end(), back_rev, out.begin(), addfirsts());
   thrust::copy(out.begin(), out.end(), output);
-  
+
 }
 
 
@@ -139,9 +151,14 @@ int main( int argc, char* argv[] )
       exit(0);
     }
 
+  // for testing - TODO!
+  int n = 10;
+  thrust::device_vector<float> d_t1(n);
+  thrust::tabulate(d_t1.begin(), d_t1.end(), RandGen());
+  
   // determine file names
   std::string infile = argv[1];
-  std::string outfile = infile.substr( 0, infile.length() - 4 ) + "_blurred_cuda.png";
+  std::string outfile = infile.substr( 0, infile.length() - 4 ) + "_blurred_thrust.png";
 
   // initialize input parameters for function
   Image input = Image( infile );
@@ -150,8 +167,6 @@ int main( int argc, char* argv[] )
   
   // time CUDA function
   auto begin = std::chrono::high_resolution_clock::now();
-  int N = input.width() * input.height();
-  size_t size = N * sizeof(float);
   cudaDeviceSynchronize();
   convolve_iir_gpu( input.data(), output_data, input.width(), input.height(), coefs);
   cudaDeviceSynchronize();
