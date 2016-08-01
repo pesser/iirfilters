dc257f4cbc784a60cd79f4c59564afefcb36f1c6
Patrick Esser <Patrick.Esser@gmx.net>
2016-08-01 12:10:23 +0200

    data collection script

---
+++ collect_sequential.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+samples=1024
+
+githash=$(git log --pretty=format:'%h' -n 1)
+exe=time_seq_deriche
+log="data_${exe}_${githash}"
+
+# header
+echo "PreInit,Horizontal,Vertical,PostInit" > ${log}
+
+# build
+make $exe
+# collect
+for((N=16;N<=16384;N*=2))
+do
+  for((sample=0;sample<samples;++sample))
+  do
+    echo $N
+    ./${exe} $N >> ${log}
+  done
+  samples=$((samples / 2))
+done
+
+echo "Done."
