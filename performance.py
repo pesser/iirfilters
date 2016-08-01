4d1fa65227bc035f3c56c5df748f2b480b23495d
Judith Massa <j.massa@mail.de>
2016-08-01 02:39:59 +0200

    fix visualization script

--- performance.py
+++ performance.py
@@ -1,13 +1,12 @@
-"""visualize performance for different versions"""
 #!/usr/bin/python
-"""visualization of rbtree measurements"""
+"""visualize performance for different versions"""
 
 import matplotlib.pyplot as plt
 import csv
 
 tsvfiles = ["cuda.stats", "thrust.stats"]
 labels = ["Pure CUDA", "Thrust with prefix scan"]
-
+color = ['blue', 'red']
 
 X = []
 for tsvfile in tsvfiles:
@@ -15,33 +14,27 @@ for tsvfile in tsvfiles:
     with open(tsvfile, 'rb') as csvfile:
         tsvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in tsvreader:
-            data[int(row[0])] = [int(row[1]), int(row[2]), float(row[2])]
+            data[int(row[0])] = [int(row[1])/1000, int(row[2])/1000, float(row[2])]
     X.append(data)
 
 
 plt.figure()
 plt.title("Performance of convolution")
 plt.xlabel("N for NxN images")
-plt.ylabel("Performance (ns)")
+plt.ylabel("Time in microseconds")
 
 
-y = sorted(X[0].keys())
-H = []
+x = sorted(X[0].keys())
 
 # sequential
-x = [X[0][k][1] for k in y]
+y = [X[0][k][1] for k in x]
+plt.plot(x, y, 'black', label="Sequential")
 
-handle, = plt.plot(x, y, 'black', label="Sequential")
-H.append(handle)
-
-color = ['#999999', '#e41a1c', '#377eb8', '#4daf4a',
-         '#984ea3', '#ff7f00', '#ffff33', '#a65628']
+# parallel
 for i in range(len(tsvfiles)):
-    x = [X[0][k][0] for k in y]
-    handle, = plt.plot(x, y, color[i], label=labels[i])
-    H.append(handle)
+    y = [X[i][k][0] for k in x]
+    plt.plot(x, y, color[i], label=labels[i])
 
 
-#plt.gca().set_ylim(0, 50000)
-plt.legend(loc=2, bbox_to_anchor=(0.9, 1), prop={'size': 10})
+plt.legend(loc=0)
 plt.savefig("performance.png")
